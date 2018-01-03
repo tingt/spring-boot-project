8ff5c3ea411963cb62c86746641ea0f82cb0971b
ttingtu <ttingtu@163.com>
2018-01-03 18:03:47 +0800

    增加README

---
+++ springboot-login/README.md
@@ -0,0 +1,7 @@
+# 框架
+springboot+mybatis+freemarker实现完整登录功能
+
+# 功能
+登录，查询/增加/修改/删除用户
+
+完整介绍可参考：http://note.youdao.com/noteshare?id=77d1dcf43814a6c8ba82b78474359af1
--- springboot-login/src/main/java/com/ttt/springboot/login/config/SpringSecurityConfig.java
+++ springboot-login/src/main/java/com/ttt/springboot/login/config/SpringSecurityConfig.java
@@ -7,12 +7,10 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * Created by tutingting on 17-11-14.
